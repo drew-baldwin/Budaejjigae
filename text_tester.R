6017bc139bee6016218c031da8defa1fc080c67e
drew-baldwin <drew.baldwin@dexcom.com>
2025-08-05 13:53:58 -0700

    Add files via upload

---
+++ text_tester.R
@@ -0,0 +1,259 @@
+
+
+
+library(tidyr)
+library(haven)
+library(lubridate)
+library(dplyr)
+library(purrr)
+library(stringr)
+library(readxl)
+rm(list = ls())
+
+
+
+
+
+
+#read in some datasets using the CRF to check approach. 
+
+ie_data=read_sas('Z:/Biostats/Studies/PTL-1000234-G7-Test-Drive-IH2/Data/Rawdata/Inclusion_Exclusion.sas7bdat')
+
+
+#install.packages("officer")
+ 
+
+library('officer')
+
+
+start_time <- Sys.time()
+
+doc=read_docx('Z:/Biostats/Studies/PTL-1000234-G7-Test-Drive-IH2/Documents/CRF/PTL1000234_Test_Drive_IH2_CRF_Final.docx')
+end_time <- Sys.time()
+
+print(end_time-start_time)
+
+#install.packages('textreadr')
+
+library('docxtractr')
+
+
+doc <- read_docx('Z:/Biostats/Studies/PTL-1000234-G7-Test-Drive-IH2/Documents/CRF/PTL1000234_Test_Drive_IH2_CRF_Final.docx')
+tables <- docx_extract_all_tbls(doc)
+
+#for each table, the row is a variable name (roughly), but for some rows there are multiple subquestions.
+#we need to write a function that will split this into multiple rows to assign variable.
+#likely can just count number of rows with check boxes, because the names for these aren't related to the text. 
+
+new=tables[[1]][6,]$V1
+new1=gsub('FORMCHECKBOX','',new) #remove check box
+str_count(new1,'No        Yes')
+
+
+
+
+#now write function that will take the tables, loop through the fields, and break apart if there are multiple sub-variables, and then assign variable names. 
+
+
+make_names=function(table_entry){
+  
+  mapped_var=list()
+  
+  #isolated to first table (IE) criteria only at this point
+  for(i in 1:10){
+    
+    #check if the row contains checkboxes. If it does then we need to make the variable name underscore the number of check box entries
+    
+    if(str_detect(table_entry[i,]$V1,'FORMCHECKBOX') & str_count(table_entry[i,]$V1,'FORMCHECKBOX')>1){
+      
+      #print('check')
+      
+      #have to account for extra checkboxes that may occur. 
+      temp=gsub('FORMCHECKBOX','',table_entry[i,]$V1) #remove checkbox
+      number_rows=str_count(temp,'No        Yes') #note that this only works for our current CRF!!
+      #print(number_rows)
+      #now, initially make our new variable the first 4 characters of the table entry
+      
+      for(j in 1:number_rows){
+        
+        new_list=list()
+
+        new_list[j]=paste(substr(table_entry[i,]$V1,1,4),'_',j)
+        
+        mapped_var <- append(mapped_var, new_list, after = i)
+
+      }
+      #mapped_var[i]=new_list
+
+     }
+    
+    else{
+      #print(table_entry[i,]$V1)
+      mapped_var[i]=substr(table_entry[i,]$V1,1,4)
+      
+    }
+    
+    
+  }
+  
+  #return(new_list)
+  return(mapped_var%>%unlist())
+  
+}
+
+make_names(tables[[1]])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#THIS PART OF THE CODE IS TO CHECK FOR TEXT MATCHING AND DISTANCE METRICS
+install.packages('text2vec')
+install.packages('stringdist')
+
+
+library('stringdist')
+library('text2vec')
+
+
+a='subjid'
+b='subid'
+
+tokens=word_tokenizer(c(a,b))
+it=itoken(tokens)
+v=create_vocabulary(it)
+
+dtm=create_dtm(it,vocab_vectorizer(v))
+sim2(dtm,method='cosine')
+
+
+stringdist(a,b,method='jw')
+
+
+my_vars=c('subid','subjid','sid','subjectid','sbjtu')
+
+new_var='subject'
+
+my_distances=list()
+
+for(i in 1:length(my_vars)){
+  my_distances[i]=stringdist(my_vars[i],new_var,method='jw')
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+doc_text=docx_summary(doc)
+
+
+#filter on only table cells, where the actual data is entered into CRF
+
+data_entries=doc_text[doc_text$content_type=='table cell',]
+
+
+#for those rows that have multiple entries (such as numbered options) we need to split
+#the text into the 
+
+
+
+
+
+#file.exists("your_document.docx")
+
+
+
+unzip('Z:/Biostats/Studies/PTL-1000234-G7-Test-Drive-IH2/Documents/CRF/PTL1000234_Test_Drive_IH2_CRF_Final.docx', exdir = "docx_unzipped")
+
+doc <- read_xml("docx_unzipped/word/document.xml")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
